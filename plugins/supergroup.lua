c5bcca4efda4f220f3a95d51234e8a226645c464
mahdibagheri <150mahdi@gmail.com>
2016-03-30 10:12:34 -0700

    Update supergroup.lua

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -1908,7 +1908,7 @@ local function run(msg, matches)
 		end
 
 		if matches[1] == 'help' and not is_owner(msg) then
-			text = "برو پی وی بات پیام بده"
+			text = "biya pv man golam"
 			reply_msg(msg.id, text, ok_cb, false)
 		elseif matches[1] == 'help' and is_owner(msg) then
 			local name_log = user_print_name(msg.from)
