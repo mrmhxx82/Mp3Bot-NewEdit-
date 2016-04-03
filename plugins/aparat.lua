83fc045a8ba26cb87185050e107ab96f86535596
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-04-03 17:01:23 +0430

    Update aparat.lua

--- plugins/aparat.lua
+++ plugins/aparat.lua
@@ -17,7 +17,7 @@ local function run(msg, matches)
 		for i = 1, #items do
 		text = text..'\n'..i..'- '..items[i].title..'  -  تعداد بازدید: '..items[i].visit_cnt..'\n    لینک: aparat.com/v/'..items[i].uid
 		end
-		text = text..'\n\n@GPMod'
+		text = text..'\n\nAqaMp3'
 		return text
 	end
 end
